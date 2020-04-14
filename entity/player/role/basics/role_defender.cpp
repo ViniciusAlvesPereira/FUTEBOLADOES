6fb906e6dae6bcb977bf504d0489f7bd9e69029d
Vinícius Alves <viniciusvap1999@gmail.com>
2020-04-14 20:55:44 -0300

    Remoção de arquivos que estavam no lugar errado

--- entity/player/role/basics/role_defender.cpp
+++ entity/player/role/basics/role_defender.cpp
@@ -8,15 +8,12 @@ Role_Defender::Role_Defender() {
 }
 
 void Role_Defender::initializeBehaviours(){
-    // Aqui são inseridos os behaviours possíveis de serem usados
-    // na ordem: ID do behaviour, instanciação dele
     usesBehaviour(BHV_SWEEPER, _bh_swp = new Behaviour_Sweeper());
     usesBehaviour(BHV_BARRIER, _bh_brr = new Behaviour_Barrier());
     usesBehaviour(BHV_PASSING, _bh_psg = new Behaviour_Passing());
 }
 
 void Role_Defender::configure(){
-    // Aqui são setados parametros que devem ser configurados
 }
 
 void Role_Defender::run(){
@@ -42,7 +39,6 @@ bool Role_Defender::ourTeamPossession() {
     for (quint8 i = 0; i < 6; i++) {
         float distanceToBall = PlayerBus::ourPlayer(i)->distanceTo(loc()->ball());
         if (distanceToBall < 0.5) {
-            //printf("Tô com a bola, otário\n");
             return true;
         }
     }
@@ -54,7 +50,6 @@ int Role_Defender::playerWithPoss() {
         float distanceToBall = PlayerBus::ourPlayer(i)->distanceTo(loc()->ball());
         if(distanceToBall < 0.5){
             return PlayerBus::ourPlayer(i)->playerId();
-            //printf("Ei ei ei, quem roubar minha bola é gay\n");
         }
     }
     return BALLPOSS_NONE;
